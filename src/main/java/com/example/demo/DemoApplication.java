24eaacb1ea3c47213a3d5fee883733577347c259
younjh <yjh5369@gmail.com>
2022-01-17 16:06:49 +0900

    region test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("##########");
+		Region region = Regions.getCurrentRegion();
+
+		if(region == null) {
+			System.out.println("region is null");
+		}
+		else {
+			System.out.println("region is not null");
+		}
 		SpringApplication.run(DemoApplication.class, args);
+
+
 	}
+
+
 
 }
